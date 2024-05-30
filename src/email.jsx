48d8768f0dbbfa0647f96a11aaa76caeaf0bb352
selina226 <166135201+selina226@users.noreply.github.com>
2024-05-30 19:48:32 +0000

    .

--- src/email.jsx
+++ src/email.jsx
@@ -24,7 +24,7 @@ function EmailForm() {
       const currentMinute = currentTime.getMinutes();
 
       // Check if current time is scheduled send time
-      if (currentHour === 15 && currentMinute === 48 ) {
+      if (currentHour === 15 && currentMinute === 52 ) {
         sendEmail(); // Call function to send email if time is matched
       }
     }, 30000); // Check every 60000 milliseconds
